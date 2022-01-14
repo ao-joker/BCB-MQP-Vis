65fe311b6cad41d9a5a47944cad6d2ffc3de2338
ao-joker <ao-joker@users.noreply.github.com>
2022-01-13 21:49:48 -0500

    Update Home.js

--- metabolic-pathway-biovis/src/Home.js
+++ metabolic-pathway-biovis/src/Home.js
@@ -2,15 +2,20 @@ import React, { useEffect } from "react";
 import * as d3 from "d3";
 
 const Home = () => {
-    
+
+    //Here, I will make a call to all the svg related functions
     useEffect(() => {
     
     })
+
+
+    //Observe, an svg related function
+
     
     return(
         <div className="home">
             <h2>The Pathway Itself!</h2>
-            
+            <svg id="Pathway" width="4000" height="2000"></svg>
         </div>
     );
 }
