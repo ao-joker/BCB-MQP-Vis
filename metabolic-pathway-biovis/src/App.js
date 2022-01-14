0291fa734f6393d0786f7026fb4a1684fd967d61
ao-joker <ao-joker@users.noreply.github.com>
2022-01-13 20:19:06 -0500

    Personalizing the title

--- metabolic-pathway-biovis/src/App.js
+++ metabolic-pathway-biovis/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 
 function App() {
+
+  //Some constant titles
+  const acknowledgements = "BCB MQP Advisor: Dr. Lane Harrison  ;  BBT MQP Advisor: Dr. Scarlet Shell";
+
   return (
     <div className="App">
       <div className="content">
         <h1><i>Mycobacterium tuberculosis</i> Metabolic Pathway Biovisualization</h1>
-         <h2>An MQP compelted by Adrian Orszulak</h2>
+         <h3>An MQP compelted by Adrian Orszulak</h3>
+         <h4>{acknowledgements}</h4>
       </div>
     </div>
   );
