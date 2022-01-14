070ca96a781ee1a3de25c9be9fe62c9d14e3ab08
ao-joker <ao-joker@users.noreply.github.com>
2022-01-13 21:39:35 -0500

    Update Home.js

--- metabolic-pathway-biovis/src/Home.js
+++ metabolic-pathway-biovis/src/Home.js
@@ -1,9 +1,16 @@
+import React, { useEffect } from "react";
+import * as d3 from "d3";
+
 const Home = () => {
     
+    useEffect(() => {
+    
+    })
     
     return(
         <div className="home">
-            <h2>The Pathway Itself</h2>
+            <h2>The Pathway Itself!</h2>
+            
         </div>
     );
 }
